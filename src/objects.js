269e41eeb4336a75a23cb04ce1292029bf642dee
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-04-03 10:14:22 +0500

    updated

--- src/objects.js
+++ src/objects.js
@@ -1,9 +1,22 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import "react-lazy-load-image-component/src/effects/blur.css";
 let obj;
 function Objects(props) {
-  obj = props.list.results.map((element) => (
+  obj = props.list.results.map((element, index) => (
     <>
       <div>
-        <img className="img-responsive" src={element.links.download} />
+        <a className="download" href={element.links.download}>
+          Download <i class="fa fa-download"></i>
+        </a>
+
+        <LazyLoadImage
+          src={element.links.download}
+          delayTime="1500"
+          effect="blur"
+          className="img-responsive"
+          key={index}
+        />
         <p>{element.alt_description}</p>
       </div>
     </>
